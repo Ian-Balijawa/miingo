cc2441117306f1752a933129e94755d4822fa1d3
engWafula <wafulaallan5@gmail.com>
2022-11-27 15:09:25 +0300

    adding profile page and improving chat component

--- src/services/axios-config.js
+++ src/services/axios-config.js
@@ -1,6 +1,6 @@
 import * as axios from "axios";
 const instance = axios.create( {
-	baseURL: process.env.REACT_APP_BASE_URL
+	baseURL: process.env.REACT_APP_BASE_URL || "https://api1.miingoapp.com",
 } );
 
 instance.defaults.headers.common[
