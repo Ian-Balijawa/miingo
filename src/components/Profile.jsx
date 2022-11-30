821c1cd6b2489615a29205ac4112ee338e844e3e
engWafula <wafulaallan5@gmail.com>
2022-11-30 17:16:17 +0300

    adding more improvements in the profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -101,23 +101,10 @@ export default function Profile({ user }) {
           <div class="mt-20 text-center border-b pb-12">
             <h1 class="text-4xl font-medium text-gray-700">{user.name}</h1>
             <p class="font-light text-gray-600 mt-3">{user.email}</p>
-
-            <p class="mt-8 text-gray-500">
-              Solution Manager - Creative Tim Officer
-            </p>
-            <p class="mt-2 text-gray-500">University of Computer Science</p>
           </div>
 
           <div class="mt-12 flex flex-col justify-center">
-            <p class="text-gray-600 text-center font-light lg:px-16">
-              An artist of considerable range, Ryan — the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-              and records all of his own music, giving it a warm, intimate feel
-              with a solid groove structure. An artist of considerable range.
-            </p>
-            <button class="text-indigo-500 py-2 px-4  font-medium mt-4">
-              Show more
-            </button>
+ 
           </div>
         </div>
       </div>
