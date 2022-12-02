ef9c6e68057ce9bb251f5ffe35c8b4e2442d2d66
Bkroland19 <bkroland19@gmail.com>
2022-12-02 18:48:17 +0300

    status wrapper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,16 @@ import Statuses from '../components/Statuses';
 import axios from '../services/axios-config';
 import { state } from '../state';
 import { useSnapshot } from 'valtio';
+import ModalWrapper from '../components/modal/ModalWrapper';
+import { DropzoneArea } from 'material-ui-dropzone';
 
 const { useState } = React;
 
-function Home() {
+function Home({contentType}) {
   const [logout, setLogout] = useState(false);
+  const [showModal , setShowModal] = useState(false)
+  const [content, setContent] = useState('');
+
   const navigate = useNavigate();
   const snapshot = useSnapshot(state);
   const name = snapshot.user?.name;
@@ -25,6 +30,13 @@ function Home() {
   const showDropdown = () => {
     setLogout(!logout);
   };
+
+  // const fileType = () => {
+  //   if (contentType.toLowerCase() === 'photo') return ['image/*'];
+  //   if (contentType.toLowerCase() === 'article') return ['application/pdf'];
+  //   if (contentType.toLowerCase() === 'video')
+  //     return ['video/mp4', 'video/webm'];
+  // };
 
   const handleLogout = async (e) => {
     e.preventDefault();
@@ -80,7 +92,52 @@ function Home() {
           </div>
         )}
 
-        <Statuses />
+        <Statuses  handlePostStatus={()=>setShowModal(true)}/>
+        {showModal
+                  ? 
+                    (<ModalWrapper title="Upload Status"  closeModal={()=> setShowModal(false)}   bodyContent = {
+                                <div className='pt-6'>
+                                    <div>
+                                        <label className='font-semibold text-textparagraph text-sm'>
+                                          UPLOAD 
+                                        </label>
+                                          <DropzoneArea
+                                            className='g-lightgraybg'
+                                            filesLimit={1}
+                                            // useChipsForPreview={
+                                            //   contentType.toLowerCase === 'photo' ? false : true
+                                            // }
+                                            showAlerts={false}
+                                            showFileNames
+                                            maxFileSize={200000000} // 200mb limit
+                                            // acceptedFiles={fileType()}
+                                            onChange={(files) => setContent(files)}
+
+
+                                            
+                                          />
+
+
+                                          
+                                    </div>
+                              </div>
+        } 
+        
+        
+        footer={true}
+                                            footerContent={
+                                              <>
+                                                <button
+                                                  className='w-full h-14 falsefocus:shadow-outline  bg-regal-orange text-white font-semibold font-display text-xl px-6 py-3 rounded-md shadow hover:bg-navyblue outline-none focus:outline-none mr-1  ease-linear transition-all duration-150'
+                                                  type='submit'
+                                                  // onClick={(e) => handleSubmit(e)}
+                                                  // disabled={loading}
+                                                >
+                                                  {/* {loading ? <PuffLoader color='white' /> : 'Submit'} */}
+                                                  Submit
+                                                </button>
+                                              </>
+                                            }/> )  : null }
 
         <main className="relative flex space-x-2 pr-3 pb-10 ">
           {/* chat */}
