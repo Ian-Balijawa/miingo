d107d628df8e881195778b76b7d063891e90a077
ianbalijawa <ianbalijawa@gmail.com>
2022-12-05 21:46:34 +0300

    remove previous base url

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -66,12 +66,12 @@ function Posts() {
           user={post.user}
           image={
             post.image
-              ? `https://api1.miingoapp.com/post/stream-video?streamFile=${post.image}`
+              ? `https://backend-miingo.herokuapp.com/post/stream-video?streamFile=${post.image}`
               : null
           }
           video={
             post.video
-              ? `https://api1.miingoapp.com/post/stream-video?streamFile=${post.video}`
+              ? `https://backend-miingo.herokuapp.com/post/stream-video?streamFile=${post.video}`
               : null
           }
           likes={post.likes.length}
