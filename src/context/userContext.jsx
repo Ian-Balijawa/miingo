9d1b00dfea792b1ff28a46e0af3969b7c622f5ad
ianbalijawa <ianbalijawa@gmail.com>
2022-12-20 19:29:41 +0300

    remove potential useUser hook bug

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,18 +1,7 @@
 import { createContext } from 'react';
-import { useUser } from '../hooks/useUser';
 
 export const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useUser();
-  return (
-    <UserContext.Provider
-      value={{
-        user,
-        setUser
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
+  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
 };
--- src/services/axios-config.js
+++ src/services/axios-config.js
@@ -1,6 +1,7 @@
 import * as axios from "axios";
 
 const instance = axios.create( {
+	// baseURL: "https://backend-miingo.herokuapp.com/",
 	baseURL: "https://backend-miingo.herokuapp.com/",
 } );
 
